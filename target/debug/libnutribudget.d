3fc2eb9c0c5fbd6724f359411afd684675b4a08a
NamazziJosephine <namazzijosephine333@gmail.com>
2026-06-12 10:22:39 +0000

    Remove build artifacts, add gitignore

--- target/debug/libnutribudget.d
+++
@@ -1,1 +0,0 @@
-/workspaces/Digital-Business-exam/target/debug/libnutribudget.rlib: /workspaces/Digital-Business-exam/src/lib.rs
--- target/debug/nutribudget.d
+++
@@ -1,1 +0,0 @@
-/workspaces/Digital-Business-exam/target/debug/nutribudget: /workspaces/Digital-Business-exam/src/lib.rs /workspaces/Digital-Business-exam/src/main.rs
